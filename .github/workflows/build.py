fd6b3fbf65a39a3013bfd0a30edda6bd8226c220
s.cobb.2022 <sjcobb2003@gmail.com>
2021-08-12 20:31:49 +0800

    edits

--- .github/workflows/build.py
+++ .github/workflows/build.py
@@ -3,25 +3,10 @@ import sys
 import getopt
 
 
-def main(argv):
-    inputfile = ''
-    outputfile = ''
-    try:
-        opts, args = getopt.getopt(argv, "hi:o:", ["ifile=", "ofile="])
-    except getopt.GetoptError:
-        print('test.py -i <inputfile> -o <outputfile>')
-        sys.exit(2)
-    for opt, arg in opts:
-        if opt == '-h':
-            print('test.py -i <inputfile> -o <outputfile>')
-            sys.exit()
-        elif opt in ("-i", "--ifile"):
-            inputfile = arg
-        elif opt in ("-o", "--ofile"):
-            outputfile = arg
-    print('Input file is "', inputfile)
-    print('Output file is "', outputfile)
-
+def main(file):
+    print("the file is", file)
+    if(file.endswith(".yml")):
+        print('it ends with yml')
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    main(sys.argv[1])
