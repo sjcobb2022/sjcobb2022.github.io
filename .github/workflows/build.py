2527177b187fddfa021e7fdef64ff681b7f679c9
s.cobb.2022 <sjcobb2003@gmail.com>
2021-08-12 22:09:56 +0800

    edits

--- .github/workflows/build.py
+++ .github/workflows/build.py
@@ -7,6 +7,8 @@ def main(file, directory):
 
     if(file.endswith(".md") and not file.startswith('.github') and ntpath.basename(file) != "index.md"):
 #         print("SUCCESS!!")
+        print(directory+"/"+file)
+        print(directory+"/markdown/"+ntpath.basename(file))
         os.rename(directory+"/"+file, directory+"/markdown/"+ntpath.basename(file))
 
 
