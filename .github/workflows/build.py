3b5f0deb70d27d71e49421ae7eb46486612b4442
s.cobb.2022 <sjcobb2003@gmail.com>
2021-08-12 21:26:56 +0800

    edits

--- .github/workflows/build.py
+++ .github/workflows/build.py
@@ -7,6 +7,7 @@ def main(file, directory):
     print("the file is", file)
     print('the directory is ', directory)
     if(file.endswith(".md") and not file.startswith('.github')):
+        print(re)
         x = re.search("(\/(?!.*\/)).*$", file)
         print(x)
         print("SUCCESS!!!!")
