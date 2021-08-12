fce37e6bc536f556d5ba27f9fe90e8c28a85a9de
s.cobb.2022 <sjcobb2003@gmail.com>
2021-08-12 22:20:31 +0800

    cmon

--- .github/workflows/build.py
+++ .github/workflows/build.py
@@ -9,8 +9,9 @@ def main(file, directory):
 #         print("SUCCESS!!")
         print(directory+"/"+file)
         print(directory+"/markdown/"+ntpath.basename(file))
-        print(os.path.dirname(os.path.realpath(file)))
+#         print(os.path.dirname(os.path.realpath(file)))
         os.rename(directory+"/"+file, directory+"/markdown/"+ntpath.basename(file))
+        os.lstdir(directory+"/markdown/)
 
 
 if __name__ == "__main__":
