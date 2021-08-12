b05e07abb9498d98836d42fc1df460fb459186d1
s.cobb.2022 <sjcobb2003@gmail.com>
2021-08-12 21:02:41 +0800

    edits

--- .github/workflows/build.py
+++ .github/workflows/build.py
@@ -1,12 +1,15 @@
 import os
 import sys
 import getopt
+import re
 
-
-def main(file):
+def main(file, directory):
     print("the file is", file)
-    if(file.endswith(".yml")):
-        print('it ends with yml')
+    print(os.getcwd())
+    if(file.endswith(".jpg") and not file.startswith('.github')):
+        x = re.search("(\/(?!.*\/)).*$", file)
+        os.rename(file, directory)
+
 
 if __name__ == "__main__":
-    main(sys.argv[1])
+    main(sys.argv[1], sys.argv[2])
