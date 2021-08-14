fd665697b3c3a7fee369d4f59b97553c78554a6e
s.cobb.2022 <sjcobb2003@gmail.com>
2021-08-14 18:30:53 +0800

    added physics notes

--- .github/workflows/build.py
+++ .github/workflows/build.py
@@ -6,7 +6,10 @@ import ntpath
 def main(file, directory):
     if (file.endswith(".md") and not file.startswith('.github') and ntpath.basename(file) != "index.md"):
         os.rename(directory + "/" + file, directory + "/markdown/" + ntpath.basename(file))
-        print(os.listdir(directory+"/markdown")
+        dirs = os.listdir(directory)
+
+        for file in dirs:
+           print(file)
 
         f = open(directory + "/index.md", 'a')
         f.write(f"<br>"
