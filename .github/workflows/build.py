c6ccd74b287777da1f0e5c630f85f043630d4e0c
s.cobb.2022 <sjcobb2003@gmail.com>
2021-08-17 12:41:27 +0800

    edited index.md

--- .github/workflows/build.py
+++ .github/workflows/build.py
@@ -8,6 +8,9 @@ def main(file, directory):
         os.rename(directory + "/" + file, directory + "/markdown/" + ntpath.basename(file))
         print(directory + "/" + file, directory + "/markdown/" + ntpath.basename(file))
 
+
+        print([x[0] for x in os.walk(directory+"/markdown")])
+
         f = open(directory + "/index.md", 'a')
         f.write(f">[{os.path.splitext(ntpath.basename(file))[0]}]({'/markdown/' + os.path.splitext(ntpath.basename(file))[0]})")
         f.close()
