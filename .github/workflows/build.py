0d982f0ff2fb8ac2504129b75bc56f8ee90ff02b
s.cobb.2022 <sjcobb2003@gmail.com>
2021-09-26 12:25:41 +0800

    build 2.0

--- .github/workflows/build.py
+++ .github/workflows/build.py
@@ -45,7 +45,7 @@ def main(file, directory):
 
             for fn in files:
                 print(f"[{file_location_array[-1]}](/{file_location_array[-1]})\n\n")
-                index.write(f"[{file_location_array[-1]}]({file_location_array[-1]})\n\n")
+                index.write(f"[{ntpath.basename(file_location_array[-1])}]({ntpath.basename(file_location_array[-1])})\n\n")
 
 
 
